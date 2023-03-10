7db67b41b259ed889ce4fe2f2dacfe3be8cfac00
smash3-11 <127226435+smash3-11@users.noreply.github.com>
2023-03-10 21:16:55 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ let menu = document.querySelector ('.menu');
 
 
 btnMenu.addEventListener('click', function() {
+    btnMenu.classList.toggle('active');
     menu.classList.toggle('active');
 })
+// let btnMenu = document.querySelector('.burger')
+// let navMenu = document.querySelector('.wrap-top-right')
+
+// btnMenu.addEventListener('click', function() {
+//     navMenu.classList.toggle('active')
+//     btnMenu.classList.toggle('active')
+// })
